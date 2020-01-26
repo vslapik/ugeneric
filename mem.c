399166d890c3a1cf746304b8e2509983c6b5b84f
Vasili Slapik <vslapik@gmail.com>
2020-01-26 19:08:15 +0300

    Make ubuffer_reserve_capacity public

--- mem.c
+++ mem.c
@@ -116,7 +116,7 @@ void *umemdup(const void *src, size_t n)
     return memcpy(umalloc(n), src, n);
 }
 
-static void _reserve_capacity(ubuffer_t *buf, size_t new_capacity)
+void ubuffer_reserve_capacity(ubuffer_t *buf, size_t new_capacity)
 {
     UASSERT_INTERNAL(buf->data_size <= buf->capacity);
     if (buf->capacity < new_capacity)
@@ -134,7 +134,7 @@ void ubuffer_append_data(ubuffer_t *buf, const void *data, size_t size)
     UASSERT_INPUT(buf);
     UASSERT_INPUT(data);
 
-    _reserve_capacity(buf, buf->data_size + size);
+    ubuffer_reserve_capacity(buf, buf->data_size + size);
     memcpy((char *)buf->data + buf->data_size, data, size);
     buf->data_size += size;
 }
@@ -144,7 +144,7 @@ void ubuffer_append_memchunk(ubuffer_t *buf, const umemchunk_t *chunk)
     UASSERT_INPUT(buf);
     UASSERT_INPUT(chunk);
 
-    _reserve_capacity(buf, buf->data_size + chunk->size);
+    ubuffer_reserve_capacity(buf, buf->data_size + chunk->size);
     memcpy((char *)buf->data + buf->data_size, chunk->data, chunk->size);
     buf->data_size += chunk->size;
 }
@@ -153,7 +153,7 @@ void ubuffer_append_byte(ubuffer_t *buf, char byte)
 {
     UASSERT_INPUT(buf);
 
-    _reserve_capacity(buf, buf->data_size + 1);
+    ubuffer_reserve_capacity(buf, buf->data_size + 1);
     ((char *)buf->data)[buf->data_size] = byte;
     buf->data_size += 1;
 }
@@ -164,7 +164,7 @@ void ubuffer_append_string(ubuffer_t *buf, const char *str)
     UASSERT_INPUT(str);
 
     size_t slen = strlen(str);
-    _reserve_capacity(buf, buf->data_size + slen);
+    ubuffer_reserve_capacity(buf, buf->data_size + slen);
     memcpy((char *)buf->data + buf->data_size, str, slen);
     buf->data_size += slen;
 }
@@ -176,12 +176,6 @@ void ubuffer_null_terminate(ubuffer_t *buf)
     {
         ubuffer_append_byte(buf, '\0');
     }
-}
-
-void ubuffer_reset(ubuffer_t *buf)
-{
-    UASSERT_INPUT(buf);
-    buf->data_size = 0;
 }
 
 int umemchunk_fprint(umemchunk_t m, FILE *out)
@@ -203,7 +197,7 @@ void umemchunk_serialize(umemchunk_t m, ubuffer_t *buf)
     const char *hex = "0123456789abcdef";
 
     ubuffer_append_data(buf, "mem:", 4);
-    _reserve_capacity(buf, buf->data_size + 2 * m.size);
+    ubuffer_reserve_capacity(buf, buf->data_size + 2 * m.size);
     for (size_t i = 0; i < m.size; i++)
     {
         unsigned char d = ((unsigned char *)m.data)[i];
--- mem.h
+++ mem.h
@@ -19,24 +19,31 @@ static inline void *uzalloc(size_t size) {return ucalloc(size, 1);}
 
 #define BUFFER_INITIAL_CAPACITY 16
 
+// Handy abstraction for simple memory buffer operations, was not intended to
+// be fully opaque so all the fields are public.
 typedef struct {
     void *data;
     size_t data_size;
     size_t capacity;
 } ubuffer_t;
 
+// Just for simplifying passing pointer together with size of the data it
+// points to.
 typedef struct {
     void *data;
     size_t size;
 } umemchunk_t;
 
+void ubuffer_reserve_capacity(ubuffer_t *buf, size_t new_capacity);
 void ubuffer_append_data(ubuffer_t *buf, const void *data, size_t size);
 void ubuffer_append_memchunk(ubuffer_t *buf, const umemchunk_t *chunk);
 void ubuffer_append_buffer(ubuffer_t *buf, const ubuffer_t *data);
 void ubuffer_append_byte(ubuffer_t *buf, char byte);
 void ubuffer_append_string(ubuffer_t *buf, const char *str);
 void ubuffer_null_terminate(ubuffer_t *buf);
-void ubuffer_reset(ubuffer_t *buf);
+
+static inline void ubuffer_reset(ubuffer_t *buf)   {buf->data_size = 0;}
+static inline void ubuffer_destroy(ubuffer_t *buf) {ufree(buf->data);}
 
 char *umemchunk_as_str(umemchunk_t m);
 void umemchunk_serialize(umemchunk_t m, ubuffer_t *buf);
